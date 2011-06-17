8aecd49ea95700bd000bcd5da63f50f92c1a3dbe
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-06-17 10:43:03 +0100

    New Xcode projects for a Cocoa Touch static library and a Cocoa framework
    Created using Xcode 4.2, build 4C104.

---
+++ objc/cocoa-framework/YAJLTests/YAJLTests.m
@@ -0,0 +1,32 @@
+//
+//  YAJLTests.m
+//  YAJLTests
+//
+//  Created by Roy Ratcliffe on 17/06/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import "YAJLTests.h"
+
+@implementation YAJLTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in YAJLTests");
+}
+
+@end
