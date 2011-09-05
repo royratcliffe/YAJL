9c3c5ec5927e4f374d12637db0fb2dbe979f22f8
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-09-05 18:16:25 +0100

    Use Apple-generic versioning

--- objc/YAJL/Versioning.h
+++ objc/YAJL/Versioning.h
@@ -1,9 +1,9 @@
-/* YAJL YAJL.h
+/* YAJL Versioning.h
  *
- * Copyright © 2010, 2011, Roy Ratcliffe, Pioneering Software, United Kingdom
+ * Copyright © 2011, Roy Ratcliffe, Pioneering Software, United Kingdom
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ * of this software and associated documentation files (the “Software”), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
@@ -12,9 +12,9 @@
  *	The above copyright notice and this permission notice shall be included in
  *	all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EITHER
+ * THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON*INFRINGEMENT. IN NO
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
  * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
  * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
@@ -22,8 +22,15 @@
  *
  ******************************************************************************/
 
-#import <YAJL/YAJLParser.h>
-#import <YAJL/YAJLGenerator.h>
-#import <YAJL/YAJLErrorDomain.h>
+#import <Foundation/Foundation.h>
 
-#import <YAJL/Versioning.h>
+extern const unsigned char kYAJLVersionString[];
+extern const double kYAJLVersionNumber;
+
+/*!
+ * Answers the current Apple-generic versioning-formatted version string. The
+ * version string has been trimmed. It has no leading or trailing whitespace or
+ * newlines. Note that the raw C-based version string contrastingly has a single
+ * terminating newline character.
+ */
+NSString *YAJLVersionString(void);
